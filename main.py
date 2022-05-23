407079d4026dc37fda60fe414c1e85052b9437e8
Envy <anastasiia.dyka.qa@gmail.com>
2022-05-23 15:57:53 +0300

    4

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ elif b > a:
 elif a == b:
     print("something about equality")
 
+a = int(input("a:"))
+    b = int(input("b:"))
+    c = int(input("c:"))
+    d = str(a)
+
+    while a < b:
+        print("sad message")
+        a += c
+        d = d + " " + str(a)
+        continue
+
+    else:
+        print("unbelievable")
+        print("history of increments", d)
+
