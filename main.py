7f804c8c711799f21277a2c484bfe65b211fbadc
Envy <anastasiia.dyka.qa@gmail.com>
2022-05-23 15:44:23 +0300

    3

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ Time = input("When will you start working in a QA position?")
 print(f"Hi user. Your name is {Name}. Your goal is {Goal}. "
       f"You will start working in a QA position {Time}.")
 
+a = input("a:")
+b = input("b:")
+c = input("c:")
+if a > b:
+    print("so sad message")
+elif b > a:
+    if b > c:
+        print("absolutely happy message")
+    else:
+        print("not so sad message")
+elif a == b:
+    print("something about equality")
+
