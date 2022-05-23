bac07e483701a2596a2a876aa98efe6bcd797f51
Envy <anastasiia.dyka.qa@gmail.com>
2022-05-23 15:43:23 +0300

    2

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ print(str)
 print(str.lower())
 print(str.upper())
 print(str.title())
+
+Name = input("What is your name?")
+Goal = input("What is your goal?")
+Time = input("When will you start working in a QA position?")
+print(f"Hi user. Your name is {Name}. Your goal is {Goal}. "
+      f"You will start working in a QA position {Time}.")
+
